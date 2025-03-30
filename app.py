0e4bde86dce03146ab381ef8c81e35ea8274b8c8
Om <80588362+om-blip@users.noreply.github.com>
2025-03-30 16:27:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import os
 from dotenv import load_dotenv
 import requests
 
-GOOGLE_API_KEY = st.secrets("GOOGLE_API_KEY")
-SERPER_API_KEY = st.secrets("SERPER_API_KEY")
+GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+SERPER_API_KEY = st.secrets["SERPER_API_KEY"]
 
 # Gemini language model
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=GOOGLE_API_KEY)
